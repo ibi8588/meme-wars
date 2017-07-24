6d25d8c1e178b3c50ece02f57e3a1827a6ebd774
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-07-24 16:22:32 -0700

    edit path working

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -6,22 +6,33 @@ class MemesController < ApplicationController
   def show
     @meme = Meme.find(params[:id])
   end
+
   def new
     @meme = Meme.new
   end
+
   def create
     @meme = Meme.new(meme_params)
     @meme.save
       redirect_to @meme
   end
+
   def destroy
     @meme = Meme.find(params[:id])
     @meme.delete
     redirect_to root_path
   end
+
   def edit
     @meme = Meme.find(params[:id])
   end
+
+  def update
+    @meme = Meme.find(params[:id])
+    @meme.update_attributes(meme_params)
+    redirect_to meme_path(@meme)
+  end
+
   private
   def meme_params
     params.require(:meme).permit(:title, :image)
