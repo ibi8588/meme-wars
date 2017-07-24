7d66bd0a603dbd9f1265b2f12269d161a5dde500
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-07-24 10:46:00 -0700

    meme create working

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -6,4 +6,16 @@ class MemesController < ApplicationController
   def show
     @meme = Meme.find(params[:id])
   end
+  def new
+    @meme = Meme.new
+  end
+  def create
+    @meme = Meme.new(meme_params)
+    @meme.save
+      redirect_to @meme
+  end
+  private
+  def meme_params
+    params.require(:meme).permit(:title, :image)
+  end
 end
