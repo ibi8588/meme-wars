0685b68c17cf0ea9cca27582f8bbf4cca2cbacd4
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-07-25 10:32:42 -0700

    users are wokring, with a username field

--- app/controllers/clearance/users_controller.rb
+++ app/controllers/clearance/users_controller.rb
@@ -1,5 +1,59 @@
 class Clearance::UsersController < Clearance::BaseController
-    before_action :require_login, except: [:new, :create]
-    before_action :correct_user, only: [:edit, :update, :destroy]
-    
+  if respond_to?(:before_action)
+    before_action :redirect_signed_in_users, only: [:create, :new]
+    skip_before_action :require_login, only: [:create, :new], raise: false
+    skip_before_action :authorize, only: [:create, :new], raise: false
+  else
+    before_filter :redirect_signed_in_users, only: [:create, :new]
+    skip_before_filter :require_login, only: [:create, :new], raise: false
+    skip_before_filter :authorize, only: [:create, :new], raise: false
+  end
+
+  def new
+    @user = user_from_params
+    render template: "users/new"
+  end
+
+  def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      render template: "users/new"
+    end
+  end
+
+  private
+
+  def redirect_signed_in_users
+    if signed_in?
+      redirect_to Clearance.configuration.redirect_url
+    end
+  end
+
+  def url_after_create
+    Clearance.configuration.redirect_url
+  end
+
+  def user_from_params
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+    username = user_params.delete(:username)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.email = email
+      user.password = password
+      user.username = username
+    end
+  end
+
+  def user_params
+    params[Clearance.configuration.user_parameter] || Hash.new
+  end
+
+  def permit_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723193630) do
+ActiveRecord::Schema.define(version: 20170725013500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170723193630) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
