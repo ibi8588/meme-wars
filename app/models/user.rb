af29b18999d8b88b76f2fd4ca6cab0884b328d9a
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-07-26 18:01:48 -0700

    created favorites and got it functioning properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
   has_many :memes
+  has_many :favorites
+  has_many :favorite_memes, through: :favorites, source: :favorited, source_type: 'Meme'
 end
