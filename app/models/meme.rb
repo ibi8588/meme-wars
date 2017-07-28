a921f5e8fd0b391d844425ca40151f6cb910cfe1
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-07-28 02:16:55 -0700

    added ways for users to act as voter, and memes to be voted on

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,4 +1,5 @@
 class Meme < ApplicationRecord
+  acts_as_votable
   belongs_to :user
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :email, :username
 
+  acts_as_voter
   has_many :memes
   has_many :favorites
   has_many :favorite_memes, through: :favorites, source: :favorited, source_type: 'Meme'
