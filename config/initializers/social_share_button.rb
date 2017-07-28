8a87384b5827aa3b5955696d2513696c328037ce
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-07-28 02:05:29 -0700

    added gemfile for acts-as-likable

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,5 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus tumblr pinterest email reddit)
+  config.allow_sites = %w(twitter facebook google_plus weibo qq douban google_bookmark
+                          delicious tumblr pinterest email linkedin wechat vkontakte
+                          xing reddit hacker_news telegram odnoklassniki)
 end
