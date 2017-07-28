bf5b78501784ce057d8f76a6684edad492821a28
Ibrahim Aldridge <ibi8588@Ibrahims-MacBook-Pro.local>
2017-07-28 11:43:26 -0700

    added initializer and user model method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
   include Clearance::User
 
+  def self.create_with_omniauth(auth)
+
+  user = find_or_create_by(uid: auth['uid'], provider:  auth['provider'])
+  user.email = “#{auth[‘uid’]}@#{auth[‘provider’]}.com”
+  user.password = auth['uid']
+  user.name = auth['info']['name']
+  if User.exists?(user)
+    user
+  else
+    user.save!
+    user
+  end
+end
+
   validates_uniqueness_of :email, :username
 
   acts_as_voter
